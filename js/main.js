763233128213b894f14542707dc9335ff4526bc1
antonynaumovic <antonynaumovic@gmail.com>
2019-06-01 20:05:49 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,40 +1,31 @@
 const menuBtn = document.querySelector('.menu-btn');
 
-const menu = document.querySelector('.menu');
-
-const menuNav = document.querySelector('.menu-nav');
-
-const menuBranding = document.querySelector('.menu-branding');
-
-const navItems = document.querySelectorAll('.nav-item');
-
-
-
-//Initial State
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
+
 function toggleMenu() {
     if(!showMenu)
     {
         menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-
+        openNav()
         showMenu = true;
     }
+
     else
     {
         menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-
+        closeNav()
         showMenu = false;
     }
 }
